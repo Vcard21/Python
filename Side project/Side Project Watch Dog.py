1f517a29d4930935573b570b590320fc2c00c1ad
Vcard21 <mike55554891@gmail.com>
2026-08-14 20:54:51 +0800

    add path and open file function
    -fix Path bug
    -add with open function

--- Side project/Side Project Watch Dog.py
+++ Side project/Side Project Watch Dog.py
@@ -1,2 +1,10 @@
-import os
 import hashlib
+from pathlib import Path
+
+file_location = Path(r"C:\Users\\Desktop\python-training")
+for file in file_location.rglob("*.py"):
+    file_bytes = file.read_bytes()
+    hash_num = hashlib.sha256(file_bytes).hexdigest()
+with open('data list.txt','rb') as f:
+    content = f.readlines()
+    print(content)
