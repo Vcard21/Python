b173c7e09a848e680e196b92f541cee3c5f01afc
Vcard21 <mike55554891@gmail.com>
2026-08-15 21:10:27 +0800

    function database
    -add sqlite3 to storage data
    -using connect to create database file
    -cursor function to control database command

--- Side project/Side Project Watch Dog.py
+++ Side project/Side Project Watch Dog.py
@@ -1,10 +1,17 @@
 import hashlib
 from pathlib import Path
+import sqlite3
 
 file_location = Path(r"C:\Users\\Desktop\python-training")
+database = sqlite3.connect('WatchDog.db')
+directory = database.cursor()
+directory.execute(f"CREATE TABLE IF NOT EXISTS WatchDog(id {hash_num} primary key auto_increment)")""")
 for file in file_location.rglob("*.py"):
     file_bytes = file.read_bytes()
     hash_num = hashlib.sha256(file_bytes).hexdigest()
-with open('data list.txt','rb') as f:
+with open('data list.txt',encoding='utf-8') as f:
     content = f.readlines()
-    print(content)
+    directory.execute(list(content)[0],[100])
+    database.commit()
+database.close()
+
