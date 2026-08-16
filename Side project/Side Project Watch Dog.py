a67315a2b05ad7d76a8c9578031933d3d93bd007
Vcard21 <mike55554891@gmail.com>
2026-08-16 23:46:42 +0800

    add database
    -add database function
    -create a table to storage data
    -using execute to confirm the old data are exist or not

--- Side project/Side Project Watch Dog.py
+++ Side project/Side Project Watch Dog.py
@@ -1,17 +1,29 @@
 import hashlib
 from pathlib import Path
 import sqlite3
+from select import select
 
 file_location = Path(r"C:\Users\\Desktop\python-training")
 database = sqlite3.connect('WatchDog.db')
 directory = database.cursor()
-directory.execute(f"CREATE TABLE IF NOT EXISTS WatchDog(id {hash_num} primary key auto_increment)")""")
+directory.execute(
+    """
+    CREATE TABLE IF NOT EXISTS WatchDog
+    (id INTEGER primary key auto_increment,
+    New hash TEXT,
+    )
+"""
+)
 for file in file_location.rglob("*.py"):
     file_bytes = file.read_bytes()
     hash_num = hashlib.sha256(file_bytes).hexdigest()
-with open('data list.txt',encoding='utf-8') as f:
-    content = f.readlines()
-    directory.execute(list(content)[0],[100])
-    database.commit()
+    directory.execute(
+        "SELECT COUNT(1) FROM WatchDog WHERE New hash = ?",
+        
+    )
+    with open('data list.txt','rb') as f:
+        content = f.readlines()
+        directory.execute(hash_num)
+        database.commit()
 database.close()
 
