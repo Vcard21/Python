69d6f0daa6a9c8019be32abc0f51a87df30df797
Vcard21 <mike55554891@gmail.com>
2026-08-22 00:49:06 +0800

    add sqlite data
    -add sqlite data
    -Next change is  if else to tell where data will be there

--- Side project/Side Project Watch Dog.py
+++ Side project/Side Project Watch Dog.py
@@ -1,7 +1,6 @@
 import hashlib
 from pathlib import Path
 import sqlite3
-from select import select
 
 file_location = Path(r"C:\Users\\Desktop\python-training")
 database = sqlite3.connect('WatchDog.db')
@@ -10,20 +9,17 @@ directory.execute(
     """
     CREATE TABLE IF NOT EXISTS WatchDog
     (id INTEGER primary key auto_increment,
-    New hash TEXT,
+    New hash,
+    Old hash,
     )
 """
 )
 for file in file_location.rglob("*.py"):
     file_bytes = file.read_bytes()
     hash_num = hashlib.sha256(file_bytes).hexdigest()
-    directory.execute(
-        "SELECT COUNT(1) FROM WatchDog WHERE New hash = ?",
-        
-    )
     with open('data list.txt','rb') as f:
         content = f.readlines()
-        directory.execute(hash_num)
+        directory.execute("INSERT INTO WatchDog (New hash) VALUES (hash_num, '{}')".format(hash_num))
         database.commit()
 database.close()
 
