4a67db6a54309b3aca8698d773ba7c5ceb3dbca3
Vcard21 <mike55554891@gmail.com>
2026-08-23 01:31:14 +0800

    made design decision
    -decide what should do in next stage
    -add baseline to compare new data that can get in next phase

--- Side project/Side Project Watch Dog.py
+++ Side project/Side Project Watch Dog.py
@@ -2,21 +2,38 @@ import hashlib
 from pathlib import Path
 import sqlite3
 
+# Phase 1
+# import Path and hashlib to connect and give sha256 value
 file_location = Path(r"C:\Users\\Desktop\python-training")
 database = sqlite3.connect('WatchDog.db')
+# 建立檔案
 directory = database.cursor()
+# 建立游標
 directory.execute(
+    # 建立內部資料表
     """
     CREATE TABLE IF NOT EXISTS WatchDog
     (id INTEGER primary key auto_increment,
     New hash,
+    Path,
     Old hash,
     )
 """
 )
+baseline = True
 for file in file_location.rglob("*.py"):
+    # Path的功能rglob去遍歷那個路徑的某一檔案，遍歷什麼檔案用（）內指定
     file_bytes = file.read_bytes()
+    # 抓到的檔案py檔案讀取後用bytes的方式呈現
     hash_num = hashlib.sha256(file_bytes).hexdigest()
+    # 把用bytes方式呈現的file_bytes加上sha256雜湊值，雜湊值用64個十六位進制表示
+    directory.execute('INSERT INTO WatchDog(Old hash, Path) VALUES (?,?)', (hash_num, file_location))
+    # 建立第一批資料baseline
+    baseline = False
+
+
+
+    # 以下暫時不會用到
     with open('data list.txt','rb') as f:
         content = f.readlines()
         directory.execute("INSERT INTO WatchDog (New hash) VALUES (hash_num, '{}')".format(hash_num))
