ebb7daff36a493007a75880b2797007a5ea731a1
Miles_Rowan <Vcard21@users.noreply.github.com>
2026-09-02 00:44:21 +0800

    add  path confirm
    -add TODO
    -add path confirm
    -add path tool to make sure data are all file

--- Side project/Side Project Watch Dog.py
+++ Side project/Side Project Watch Dog.py
@@ -15,14 +15,14 @@ directory.execute(
     """
     CREATE TABLE IF NOT EXISTS WatchDog
     (id INTEGER primary key auto_increment,
-    Path,
-    Old hash,
+     Path TEXT NOT NULL,
+     Old hash TEXT NOT NULL,
     )
-"""
+    """
 )
 
 """
-Issue：How to determine whether baseline has been initialized !Solved!
+Issue：How to determine whether baseline has been initialized 
 
 原設計：透過第一筆資料是否存在判斷baseline是否建立。
 
@@ -31,6 +31,8 @@ Issue：How to determine whether baseline has been initialized !Solved!
 TODO：研究SQLite中更適合保存／判斷baseline initialization state的方法。
 
 Update：Using fetchone tool return VALUES to determine already build baseline or not
+
+-Solved
 """
 directory.row_factory = sqlite3.Row
 # core setting to control what data will return by database(sqlite3)
@@ -50,44 +52,69 @@ if baseline is None:
         # 把用bytes方式呈現的file_bytes加上sha256雜湊值，雜湊值用64個十六位進制表示
         directory.execute('INSERT INTO WatchDog(Old hash, Path) VALUES (?,?)', (hash_num, file))
         # 建立第一批資料baseline
+        print("beseline are now created")
         database.commit()
     database.close()
 else:
     # if baseline already EXISTS,then rglob all the file again to compare the hash_num has change or not
-    for file in file_location.rglob("*.py"):
-        # Path的功能rglob去遍歷那個路徑的某一檔案，遍歷什麼檔案用（）內指定
-        file_bytes = file.read_bytes()
-        # 抓到的檔案py檔案讀取後用bytes的方式呈現
-        hash_num = hashlib.sha256(file_bytes).hexdigest()
-        # 把用bytes方式呈現的file_bytes加上sha256雜湊值，雜湊值用64個十六位進制表示
-        New_hash[file] = hash_num
-    # add current status of hash to compare baseline
-    directory.execute("SELECT Path,Old hash FROM WatchDog")
-    # select Path and Old hash
-    Old_hash = {row["Path"]: row["Old hash"] for row in directory.fetchall()}
-    # after select Path and Old hash,storage database values as dict,then two dict should be comparable
-
-    if New_hash[file] == Old_hash[Path]:
-        # 如果沒變
-        if New_hash[hash_num] == Old_hash["Old hash"]:
-            # 再度判斷hash是否有變
-            print("Unchange")
-        else:
-            directory.execute(
-                "UPDATE WatchDog SET Old hash = ?",New_hash[Path] )
-            print("Change")
-    else: # 這邊就已經代表是路徑不相符的時候，所以路徑不相符時的判斷就應該是【刪除、新增】
-        if  New_hash[file] is None: # 這邊應該是刪除，想想看，刪除應該用什麼判斷?應該是原本有但現在沒有
-            print(f"The file{Old_hash["Path"]} has been Deleted")
-        # path不等於空時，代表路徑不同，所以代表新增
-        else:
-            directory.execute("INSERT INTO WatchDog(Path,Old_hash) VALUES (?,?)", New_hash)
-            print(f"Found new file{New_hash[file]} that wasn't exist before.(Update to Database)")
+    for file in file_location.rglob("*"):
+        if not file.is_file():
+            continue
             """
             TODO:
-            add Not Null for database to make sure None value are truly null
+                rglob all file but not include Path itself
+            Solvation1:
+                using Path.is_file() tool to sparate path and file
+            -Solved
+
+            New problem:
+                database are a file too,when every changed by program itself,program will tell us the file has changed
+            Solvation2:
+                using tool Path.resolve()
+                這個工具可以把Path轉成解析後的絕對路徑
+                konwn by AI
             """
-    # 以下暫時不會用到
-    # with open('data list.txt','rb') as f:
-    #     content = f.readlines()
-    #     directory.execute("INSERT INTO WatchDog (New hash) VALUES (hash_num, '{}')".format(hash_num))
+        # 用resolve功能判定絕對路徑，如果這個路徑跟database存著的Watch dog db黨相同
+        # 那就中斷這一次的資料輸入，進入下一個循環
+        if database == file.path.resolve('python')
+            continue
+            # Path的功能rglob去遍歷那個路徑的某一檔案，遍歷什麼檔案用（）內指定
+            file_bytes = file.read_bytes()
+            # 抓到的檔案py檔案讀取後用bytes的方式呈現
+            hash_num = hashlib.sha256(file_bytes).hexdigest()
+            # 把用bytes方式呈現的file_bytes加上sha256雜湊值，雜湊值用64個十六位進制表示
+            New_hash[file] = hash_num
+        # add current status of hash to compare baseline
+        directory.execute("SELECT Path,Old hash FROM WatchDog")
+        # select Path and Old hash
+        Old_hash = {row["Path"]: row["Old hash"] for row in directory.fetchall()}
+        # after select Path and Old hash,storage database values as dict,then two dict should be comparable
+
+        if New_hash[file] == Old_hash[Path]:
+            # 如果沒變
+            if New_hash[hash_num] == Old_hash["Old hash"]:
+                # 再度判斷hash是否有變
+                print("Unchange")
+            else:
+                directory.execute(
+                    "UPDATE WatchDog SET Old hash = ?",New_hash[Path] )
+                print("Change")
+        else: # 這邊就已經代表是路徑不相符的時候，所以路徑不相符時的判斷就應該是【刪除、新增】
+            if  New_hash[file] is None: # 這邊應該是刪除，想想看，刪除應該用什麼判斷?應該是原本有但現在沒有
+                print(f"The file{Old_hash["Path"]} has been Deleted")
+            # path不等於空時，代表路徑不同，所以代表新增
+            else:
+                directory.execute("INSERT INTO WatchDog(Path,Old_hash) VALUES (?,?)", New_hash)
+                print(f"Found new file{New_hash[file]} that wasn't exist before.(Add to Database)")
+                """
+                TODO:
+                add Not Null for database to make sure None value are truly null
+
+                -Solved
+                """
+                database.commit()
+    database.close()
+# 以下暫時不會用到
+# with open('data list.txt','rb') as f:
+#     content = f.readlines()
+#     directory.execute("INSERT INTO WatchDog (New hash) VALUES (hash_num, '{}')".format(hash_num))
